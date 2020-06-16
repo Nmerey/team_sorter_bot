91f8037b198872d0ecaba0d91acc5ab04da70575
Nmerey <merey.nurlan@gmail.com>
2020-06-16 19:14:13 +0300

    test

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -123,6 +123,7 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
       session[:callback]  = payload["message"]
       session[:friend_id] = from['id']
       reply_with :message, text: "Name and Rating form 1 to 10 like so: \n Chapa 0", reply_markup: {force_reply: true, selective: true}
+      p payload
       add_friend(payload['message']['text'])
 
     elsif data[0] == 'r'
