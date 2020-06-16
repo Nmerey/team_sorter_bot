417fa1cf9b071e983c2e7182dc4d62b975294b51
Nmerey <merey.nurlan@gmail.com>
2020-06-16 16:59:41 +0300

    Selective 6

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -119,7 +119,8 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
       session[:venue_id]  = @venue.id
       session[:callback]  = payload["message"]
       session[:friend_id] = from['id']
-
+      p payload['chat']
+      p payload['message']
       Telegram.bot.send_message(chat_id: payload['chat']['id'], text: "Give Name and Rating like so: \n Chapa 0", reply_to_message_id: payload["message"]["id"], reply_markup: { force_reply: true, selective: true})
       save_context :add_friend
 
