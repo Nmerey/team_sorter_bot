1df967e415230daaa82dbe13b14b24871b01cb15
Nmerey <merey.nurlan@gmail.com>
2020-06-16 22:26:14 +0300

    Update rating view and controllers

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,24 @@
-class PlayerController < ApplicationController
+class PlayersController < ApplicationController
 	def index
-		@players = Player.all
+		@players = Player.where(friend_id: nil)
 	end
+
+	def edit
+		@player = Player.find(params[:id])
+	end
+
+	def update
+		@player = Player.find(params[:id])
+		@player.rating.nil? ? @old_rating = 0 : @old_rating = @player.rating
+		@rating = (params[:player][:rating].to_i + @old_rating) / 2
+
+		if @player.update(rating: @rating)
+			redirect_to root_path
+			flash[:success] = "Rating saved" 
+		else
+			flash[:alert] = "Not saved!"
+		end
+
+	end
+
 end
