a2db7b5a8480e93da6a5199e022b16fe0e342591
Nmerey <merey.nurlan@gmail.com>
2020-07-02 19:25:40 +0300

    Validate uniqueness of player

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,5 @@
 class Match < ApplicationRecord
   belongs_to :venue
   belongs_to :player
+  validates :player_id, uniqueness: {scope: :venue_id, message: "cant be more than one player in list"}
 end
